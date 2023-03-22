cb3e12fe76eca5101235294c2a0de10d984a0a30
Gonzalo Ibarrola <gonzaloibarrola95@gmail.com>
2023-03-22 14:36:50 -0300

    Commit clase 7

--- main.js
+++ main.js
@@ -5,3 +5,42 @@ menu.addEventListener('click', () => {
   menu.classList.toggle('open')
   nav.classList.toggle('navbar-hamburger')
 })
+
+
+// DATA ----->
+const dataPrueba = localStorage.getItem("products");
+const data = JSON.parse(dataPrueba);
+
+// CARRITO ----->
+let productos = data;
+let carrito = [];
+
+// CARRITO -----> AGREGAR AL CARRITO ----->
+function addToCart() {
+  
+}
+let cart = document.getElementById('carrito')
+
+
+// addToCart.addEventListener('click', () => {
+
+//   if (carrito.length == 0) {
+//     carrito.push(vacio++)
+//     console.log(carrito)
+//   } else {
+//     carrito.push(vacio++)
+//     console.log(carrito)
+//   }
+//   cart.innerHTML = `${carrito.length}`
+// });
+
+// cartClear.addEventListener('click', () => {
+//   carrito = carrito.length = [];
+//   console.log(carrito);
+//   cart.innerHTML = `${carrito.length}`
+// });
+
+
+
+
+
