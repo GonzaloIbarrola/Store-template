1a5272aaa63e6b823e4a1bd19bad7ff42c66036a
Gonzalo Ibarrola <gonzaloibarrola95@gmail.com>
2023-03-27 16:14:40 -0300

    Restrucuración de código y manejo de Async Await.

--- api.js
+++ api.js
@@ -1,41 +1,15 @@
 // URL API.
 const url = 'https://fakestoreapi.com/products';
 
-function renderData(data) {
-    // BUCLE PARA OBTENER LOS DATOS DE CADA OBJETO DENTRO DEL ARRAY.
-    for(let i = 0; i < data.length; i++) {
-      let title = data[i].title;
-      let image = data[i].image;
-      // let description = data[i].description;
-      // let category = data[i].category;
-      let price = data [i].price;
 
-
-      // ESTRUCTURA DE ARTICULOS EN EL HTML.
-      let articulos = document.getElementById('Articles');
-      let estructuraArticulos = `
-      <div class="article-container" id="articulo">
-          <img src="${image}" alt="" class="article-container-img">
-          <div class="article-container2">
-            <a id="article_Title" class="article_title">${title}</a>
-            <a class="article-container2-button" id="addToCart">
-              <img src="./images/icon-svg/cart-4.svg" alt="" class="article-container2-button-img">
-              <span>$${price}</span>
-            </a>
-          </div>
-        </div>
-      `;
-      articulos.innerHTML += estructuraArticulos;
-    };
-  };
-
-async function getData() {
+async function getData(renderData) {
   const data = await fetch(url);
   const json = await data.json();
   localStorage.setItem("products",JSON.stringify(json));
+  console.log(json)
   renderData(json)
 }
 
 // LLAMADA A LA API CON FETCH.
 // document.addEventListener('DOMContentLoaded', getData);
-getData()
+export {getData};
--- main.js
+++ main.js
@@ -1,46 +1,40 @@
-const menu = document.querySelector('#menu')
-const nav = document.querySelector('#hamburger')
+// const menu = document.querySelector('#menu')
+// const nav = document.querySelector('#hamburger')
 
-menu.addEventListener('click', () => {
-  menu.classList.toggle('open')
-  nav.classList.toggle('navbar-hamburger')
-})
-
+// menu.addEventListener('click', () => {
+//   menu.classList.toggle('open')
+//   nav.classList.toggle('navbar-hamburger')
+// })
 
 // DATA ----->
-const dataPrueba = localStorage.getItem("products");
-const data = JSON.parse(dataPrueba);
+import { getData } from "./api.js";
+document.addEventListener('DOMContentLoaded', getData(renderData))
 
-// CARRITO ----->
-let productos = data;
-let carrito = [];
-
-// CARRITO -----> AGREGAR AL CARRITO ----->
-function addToCart() {
-  
-}
-let cart = document.getElementById('carrito')
-
-
-// addToCart.addEventListener('click', () => {
-
-//   if (carrito.length == 0) {
-//     carrito.push(vacio++)
-//     console.log(carrito)
-//   } else {
-//     carrito.push(vacio++)
-//     console.log(carrito)
-//   }
-//   cart.innerHTML = `${carrito.length}`
-// });
-
-// cartClear.addEventListener('click', () => {
-//   carrito = carrito.length = [];
-//   console.log(carrito);
-//   cart.innerHTML = `${carrito.length}`
-// });
+function renderData(data) {
+  // BUCLE PARA OBTENER LOS DATOS DE CADA OBJETO DENTRO DEL ARRAY.
+  for(let i = 0; i < data.length; i++) {
+    let id = data[i].id;
+    let title = data[i].title;
+    let image = data[i].image;
+    // let description = data[i].description;
+    // let category = data[i].category;
+    let price = data [i].price;
 
 
-
-
-
+    // ESTRUCTURA DE ARTICULOS EN EL HTML.
+    let articulos = document.getElementById('Articles');
+    let estructuraArticulos = `
+    <div class="article-container" id="articulo">
+        <img src="${image}" alt="" class="article-container-img">
+        <div class="article-container2">
+          <a id="article_Title" class="article_title">${title}</a>
+          <a class="article-container2-button" id="addToCart" data-id="${id}" onclick="addToCart()">
+            <img src="./images/icon-svg/cart-4.svg" alt="" class="article-container2-button-img">
+            <span>$${price}</span>
+          </a>
+        </div>
+      </div>
+    `;
+    articulos.innerHTML += estructuraArticulos;
+  };
+};
